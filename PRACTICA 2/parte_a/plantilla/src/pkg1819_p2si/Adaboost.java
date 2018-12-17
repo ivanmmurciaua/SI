4e771361e9dfd854592ff89d756125379bdcbc2d
ivanmmurciaua <32136734+ivanmmurciaua@users.noreply.github.com>
2018-12-17 14:16:57 +0100

    P1: Empezando Adaboost

--- PRACTICA 2/parte_a/plantilla/src/pkg1819_p2si/Adaboost.java
+++ PRACTICA 2/parte_a/plantilla/src/pkg1819_p2si/Adaboost.java
@@ -6,12 +6,141 @@
 
 package pkg1819_p2si;
 
+import java.io.*;
+
 /**
  * 
  * @author Ivan
  */
 public class Adaboost {
     
+    final static int CDaUsar = 400;
+    final static int PruAle = 500;
     
+    
+    class CD{
+        
+        private int pixel;
+        private int umbral;
+        private int direccion;
+        private float alfa;
+        
+        CD(){
+            this.pixel = (int)(Math.random()*784);
+            this.umbral = (int)(Math.random()*255);
+            this.direccion = (int)(Math.random()*784);
+            
+            if(this.direccion % 2 == 0){
+                this.direccion = 1;
+            }
+            else{
+                this.direccion = -1;
+            }
+            
+            this.alfa = 0;
+        }
+        
+        @Override
+        public String toString(){
+            return this.pixel+" "+this.umbral+" "+this.direccion+" "+this.alfa;
+        }
+        
+        public void escribirEnFichero(CD cf, boolean switcheo, String nombrefichero){
+            
+            //PRIMER FICHERO
+            if(switcheo){
+                FileWriter fichero = null;
+                try{
+                    fichero = new FileWriter(nombrefichero);
+                    PrintWriter pw = new PrintWriter(fichero);
+                    pw.println(cf);
+                }
+                catch (IOException e) {} 
+                finally {
+                   try{
+                   if (null != fichero)
+                      fichero.close();
+                   }
+                   catch (IOException e2) {}
+                }
+            }
+            //CONTINUAMOS ESCRIBIENDO EN EL FICHERO
+            else{
+                try(FileWriter fw = new FileWriter(nombrefichero, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw)){
+                    out.println(cf);
+                }
+                catch (IOException e) {}
+            }
+        }
+        
+        public void leerDeFichero(String cf){
+            //-RUN  (LECTURA FICHERO)
+            File archivo = null;
+            FileReader fr = null;
+            BufferedReader br = null;
 
+            try {
+               archivo = new File (cf);
+               fr = new FileReader (archivo);
+               br = new BufferedReader(fr);
+
+               String linea;
+               while((linea=br.readLine())!=null)
+                  System.out.println(linea);
+            }
+            catch(IOException e){}
+            finally{
+               try{                    
+                  if( null != fr ){   
+                     fr.close();     
+                  }                  
+               }catch (IOException e2){}
+            }
+        }
+
+    }
+    
+    
+    public void Adaboost(){
+        
+        CD primero = new CD();
+        
+        
+        
+    }
+    /*
+    ** 
+    
+       BD -> 80%  TRAIN
+          -> 20%  TEST
+    
+       CD
+       +-------------------------+
+       | Píxel     ->  0-784     |
+       | Umbral    ->  0-255     |
+       | Dirección -> +1 o -1    |
+       | alfa      -> formula    |
+       +-------------------------+
+    
+       D -> Vector de pesos (Inicializa a 1/N -> N=X.length)
+       T -> Numero de CD a usar
+       
+       para i=1 hasta A-> Numero de pruebas aleatorias
+            CD = Plano al azar;
+            (El vector Err se supone que lo pasamos nosotros)
+            actualizamos su Et haciendo un sumatorio de D * Err
+            guardamos el clasificador debil con menos Et
+       finpara
+       
+       obtener
+    
+    
+    
+    */
+    
+    
+    
+    
 }
--- PRACTICA 2/parte_a/plantilla/src/pkg1819_p2si/Main.java
+++ PRACTICA 2/parte_a/plantilla/src/pkg1819_p2si/Main.java
@@ -6,6 +6,8 @@
 package pkg1819_p2si;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import static pkg1819_p2si.Adaboost.*;
 
 /**
  *
@@ -18,30 +20,83 @@ public class Main {
      */
     public static void main(String[] args) {
         
+        /*
+        
+        switch(args.length){
+            case 3 : if("-train".equals(args[0])){System.err.println("El comando -train debe tener 1 argumento solo");} else if("-run".equals(args[0])){ System.out.println("Analizando");}
+            break;
+            case 2 : if("-train".equals(args[0])){ System.out.println("Entrenando");} else if("-run".equals(args[0])){System.err.println("El comando -run debe tener 2 argumentos");}
+            break;
+            default : System.err.println("Error:\nModo de uso:\nPara entrenar Adaboost: Adaboost -train fichero.cf\nPara testear Adaboost: Adaboost -run fichero.cf imagen_prueba");
+        }
+            
+    */
+        
+        //int[] Y = new int[3200];  //0->3199
+        //int[] Y_abrigos = new int[3200];
+        //int[] Y_bolsos = new int[3200];
+        //int[] Y_camisetas = new int[3200];
+        //int[] Y_pantalones = new int[3200];
+        //int[] Y_sueters = new int[3200];
+        //int[] Y_vestidos = new int[3200];
+        //int[] Y_zapatillas = new int[3200];
+        //int[] Y_zapatos = new int[3200];
+        //Arrays.fill(Y,-1);
+        Adaboost p1 = new Adaboost();
+        p1.Adaboost();
+        
+        //Abrigos
+        //for(int i=0;i<401;i++) Y[i]=1;
+        //Bolsos
+        //for(int i=400;i<801;i++) Y[i]=1;
+        //Camisetas
+        //for(int i=800;i<1201;i++) Y[i]=1;
+        //Pantalones
+        //for(int i=1200;i<1601;i++) Y[i]=1;
+        //Sueters
+        //for(int i=1600;i<2001;i++) Y[i]=1;
+        //Vestidos
+        //for(int i=2000;i<2401;i++) Y[i]=1;
+        //Zapatillas
+        //for(int i=2400;i<2801;i++) Y[i]=1;
+        //Zapatos
+        //for(int i=2800;i<3201;i++) Y[i]=1;
+        
+        
+        //Adaboost(X,Y);   
+        
         //Cargador de la BD de SI
-        DBLoader ml = new DBLoader();
-        ml.loadDBFromPath("./db");
+        //DBLoader ml = new DBLoader();
+        //ml.loadDBFromPath("./db");
         
         //Accedo a las imagenes de bolsos
-        ArrayList d0imgs = ml.getImageDatabaseForDigit(1);
+        //ArrayList d0imgs = ml.getImageDatabaseForDigit(5);
         
         //Y cojo el decimo bolso de la bd
-        Imagen img = (Imagen) d0imgs.get(9);
+        //Imagen img = (Imagen) d0imgs.get(5);
+        //System.out.println(img);
         
         //La invierto para ilustrar como acceder a los pixels y imprimo los pixeles
         //en hexadecimal
-        System.out.print("Image pixels: ");
-        byte imageData[] = img.getImageData();
-        for (int i = 0; i < imageData.length; i++)
-        {
-            imageData[i] = (byte) (255 - imageData[i]);
-            System.out.format("%02X ", imageData[i]);
-        }
+        //System.out.print("Image pixels: ");
+        //int max = -255;
+        //int min = 255;
+        //byte imageData[] = img.getImageData();
+        //for (int i = 0; i < imageData.length; i++){
+        //    imageData[i] = (byte) (255 - imageData[i]);
+        //    if(imageData[i]+128 < min) min = imageData[i]+128;
+        //    if(imageData[i]+128 > max) max = imageData[i]+128;
+        //    System.out.print(imageData[i]+128+" ");
+        //}
         
+        //System.out.println("JAP");
+        //System.out.println(min);
+        //System.out.println(max);
+
         //Muestro la imagen invertida
-        MostrarImagen imgShow = new MostrarImagen();
-        imgShow.setImage(img);
-        imgShow.mostrar();
+        //MostrarImagen imgShow = new MostrarImagen();
+        //imgShow.setImage(img);
+        //imgShow.mostrar();
 
         
     }
